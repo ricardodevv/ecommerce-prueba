aaa6a1015c328bb16e3edf2409795c9fd4dd367a
Ricardo Quintero <ricardoaqf.dev@gmail.com>
2021-07-28 16:35:52 -0430

    header buttons

---
+++ pages/login.js
@@ -0,0 +1,18 @@
+import Layout from "../components/Layout";
+import styled from "styled-components";
+
+const LoginContainer = styled.div``;
+
+const LoginForm = styled.div``;
+
+const Login = () => {
+  return (
+    <Layout>
+      <LoginContainer>
+        <LoginForm></LoginForm>
+      </LoginContainer>
+    </Layout>
+  );
+};
+
+export default Login;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCOtv4gx-CW3URiENS9zFasya1YYaFujbc",
+  authDomain: "animal-kingdom-store.firebaseapp.com",
+  projectId: "animal-kingdom-store",
+  storageBucket: "animal-kingdom-store.appspot.com",
+  messagingSenderId: "271711585303",
+  appId: "1:271711585303:web:6d84f5dff1776b8b2e0852",
+  measurementId: "G-VB4LD43H8M",
+};
