3fa19815f1c54f2aac073fbf5cd8e85d330eeaa1
Ricardo Quintero <ricardoaqf.dev@gmail.com>
2021-08-11 08:19:14 -0430

    before pull

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,6 @@ const firebaseConfig = {
   projectId: "animal-kingdom-store",
   storageBucket: "animal-kingdom-store.appspot.com",
   messagingSenderId: "271711585303",
-  appId: "1:271711585303:web:6d84f5dff1776b8b2e0852",
-  measurementId: "G-VB4LD43H8M",
+  appId: "1:271711585303:web:fbae8377092ec7af2e0852",
+  measurementId: "G-VC3EE9FJPV",
 };
