285f0678c4db52963830e7234969df3aa2bb3763
Ricardo Quintero <ricardoaqf.dev@gmail.com>
2021-08-10 21:33:57 -0500

    firebase

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzGsBgFI_3mYF89oGG9XPsqGaaBiA-1Ks",
+  authDomain: "animalkingdom-store.firebaseapp.com",
+  projectId: "animalkingdom-store",
+  storageBucket: "animalkingdom-store.appspot.com",
+  messagingSenderId: "466347453832",
+  appId: "1:466347453832:web:1a270f3a9d31b8b7c59e3d",
+  measurementId: "G-D3BN5Y8J72",
+};
+
+firebase.initializeApp(firebaseConfig);
